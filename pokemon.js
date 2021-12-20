f448cae53f9b60fe1ae84fc3c2bdb8d8a45ee0b6
Samuel-G3 <sgarciaine@gmail.com>
2021-12-20 16:43:03 +0100

    final

---
+++ pokemon.js
@@ -0,0 +1,68 @@
+const pokelista$$ = document.querySelector(".pokelista");
+
+const MyPokemonArray = () => {
+  const pokemonArray = [];
+
+  for (let i = 1; i <= 151; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    pokemonArray.push(fetch(url).then((res) => res.json()));
+  }
+
+  Promise.all(pokemonArray).then((results) => {
+    print(results);
+  });
+};
+
+const print = (pokemonData) => {
+  for (const pokemon of pokemonData) {
+    const li$$ = document.createElement("li");
+    li$$.innerHTML = `
+        <li class="display">
+        <h2>${pokemon.name}</h2>
+         <img
+           class="pokemonimage"
+           src="${pokemon.sprites.other["official-artwork"]["front_default"]}"
+           alt="${pokemon.name}"
+         />
+         <p>Type: ${pokemon.types.map((type) => type.type.name).join(", ")} </p>
+         <p>Weight: ${pokemon.weight / 10} Kg.</p>
+         <p>Height: ${pokemon.height * 10} cm.</p>
+        <p>Number: ${pokemon.id} </p>
+        </li>`;
+    pokelista$$.appendChild(li$$);
+  }
+};
+
+MyPokemonArray();
+//________________________________________
+ 
+
+document.querySelector("#button-search").addEventListener("click", () => {
+  let busquedaPokemon = document.querySelector("#formulario").value;
+  const searchPokemon = async (busquedaPokemon) => {
+    let pokemonAPI = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${busquedaPokemon}`
+    );pokemonRES = await pokemonAPI.json();
+
+    imprimirPokemon(pokemonRES);
+  };
+  searchPokemon(busquedaPokemon);
+
+  const imprimirPokemon = (pokemonRES) => {
+    const btn = document.querySelector("#pickedPokemon");
+    btn.hidden = false;
+    btn.innerHTML = `
+    <div class="display">
+     <h2>${pokemonRES.name}</h2>
+    <img
+      class="pokemonimage"
+      src="${pokemonRES.sprites.other["official-artwork"]["front_default"]}"
+      alt="${pokemonRES.name}"
+    />
+    <p>Type: ${pokemonRES.types.map((type) => type.type.name).join(", ")} </p>
+    <p>Weight: ${pokemonRES.weight / 10} Kg.</p>
+   <p>Height: ${pokemonRES.height * 10} cm.</p>
+   <p>Number: ${pokemonRES.id} </p>
+   </div>`;
+  };
+});
